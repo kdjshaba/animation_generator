2f75af7abecdbc1cd25c122e378ddae9db317e29
Lisa <857871573@qq.com>
2016-09-05 23:40:04 +0800

    不能限制移动区域的大小

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@
  */
 
 var element = document.getElementById("object");
-
+var canvas = document.getElementById("canvas");
 var test= document.getElementById("canvas");
 //test.offsetLeft是横坐标
 //test.offsetTop是纵坐标
@@ -12,12 +12,16 @@ function move(event) {
     element.style.transform = "translate("+(event.clientX-25-test.offsetLeft)+"px,"+(event.clientY-25-test.offsetTop)+"px)";
 }
 
+//想了半天想加一个监听事件
+canvas.addEventListener("mousemove",function () {
+    element.addEventListener("mousedown", function () {
+        window.addEventListener("mousemove",move);
+        window.addEventListener("mouseup",function () {
+            window.removeEventListener("mousemove",move);
+        })
 
-element.addEventListener("mousedown", function () {
-    window.addEventListener("mousemove",move);
-    window.addEventListener("mouseup",function () {
-        window.removeEventListener("mousemove",move);
-    })
+    });
+})
 
-});
+
 
