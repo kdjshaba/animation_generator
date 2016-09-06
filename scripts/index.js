ce665e689e1c562cb0c2cd59157e7a550fd8ca2f
Lisa <857871573@qq.com>
2016-09-06 15:56:37 +0800

    限制区域功能完成

--- scripts/index.js
+++ scripts/index.js
@@ -8,20 +8,38 @@ var test= document.getElementById("canvas");
 //test.offsetLeft是横坐标
 //test.offsetTop是纵坐标
 
-function move(event) {
-    element.style.transform = "translate("+(event.clientX-25-test.offsetLeft)+"px,"+(event.clientY-25-test.offsetTop)+"px)";
+function move(event, offsetLeft, offsetTop) {
+    var x = event.clientX-offsetLeft-3-test.offsetLeft;
+    if(x > 570) {
+        x = 570;
+    } else if(x < 0 ){
+        x = 0;
+    }
+    var y = event.clientY-3-offsetTop-test.offsetTop;
+    if(y > 350 ) {
+        y = 350;
+    } else if(y < 0 ){
+        y = 0;
+    }
+        console.log(x, y)
+    element.style.transform = "translate("+x+"px,"+y+"px)";
+
 }
 
 //想了半天想加一个监听事件
-canvas.addEventListener("mousemove",function () {
-    element.addEventListener("mousedown", function () {
-        window.addEventListener("mousemove",move);
-        window.addEventListener("mouseup",function () {
-            window.removeEventListener("mousemove",move);
-        })
-
-    });
+element.addEventListener("dragstart", function (e) {
+    e.preventDefault();
 })
+element.addEventListener("mousedown", function (e) {
+    function move2() {
+        move(event, e.offsetX, e.offsetY);
+
+    }
+    window.addEventListener("mousemove", move2);
+    window.addEventListener("mouseup",function () {
+           window.removeEventListener("mousemove",move2);
+    })
+ });
 
 
 
