40c0574055d407597a341e584ba882b55db10bb5
Lisa <857871573@qq.com>
2016-09-09 10:42:11 +0800

    可定位小圆圈,解决了slider和circle同级的问题.(差拖拽功能)

--- scripts/index.js
+++ scripts/index.js
@@ -5,8 +5,12 @@
 var element = document.getElementById("object");
 //var canvas = document.getElementById("canvas");
 var test= document.getElementById("canvas");
-//test.offsetLeft是横坐标
-//test.offsetTop是纵坐标
+//test.offsetLeft是移动的相对横坐标
+//test.offsetTop是移动的相对纵坐标
+//offsetLeft是点击的位置相对被点击对象的横坐标
+//offsetTop是点击的位置相对被点击对象的纵坐标
+//clientX是点击的那个点的横坐标
+//clientY是点击的那个点的纵坐标
 
 function move(event, offsetLeft, offsetTop) {
     var x = event.clientX-offsetLeft-3-test.offsetLeft;
@@ -21,7 +25,7 @@ function move(event, offsetLeft, offsetTop) {
     } else if(y < 0 ){
         y = 0;
     }
-        console.log(x, y)
+    //console.log(x, y)
     element.style.transform = "translate("+x+"px,"+y+"px)";
 
 }
@@ -43,23 +47,62 @@ element.addEventListener("mousedown", function (e) {
     })
  });
 
-//每点击一下就增加一个圆圈
+
+
+//每点击一下就增加一个圆圈;
+var middle = document.getElementById("middle");
 var slider = document.getElementById("slider");
+var event = slider.event;
+function generate(event) {
 
-
-function generate() {
     var circle = document.createElement("div");
     circle.className = "circle";
-    slider.appendChild(circle);
+    console.log(event)
+    var x = event.offsetX - 5;
+    circle.style.left = x + "px";
+    middle.appendChild(circle);
 
 };
 
-slider.addEventListener("click", function () {
-    generate ();
+slider.addEventListener("click", function (event) {
+    generate (event);
 });
+
+/*
+//可以移动圆圈的功能
+var spot = document.getElementByClassName("circle")
+function move3(event, offsetLeft) {
+    var x = event.clientX-offsetLeft-3-test.offsetLeft;
+    if(x > 570) {
+        x = 570;
+    } else if(x < 0 ){
+        x = 0;
+    }
+    var y = event.clientY;
+    //console.log(x, y)
+    spot.style.transform = "translate("+x+"px,"+y+"px)";
+
+}
+
+//阻挡原始拖动的产生
+spot.addEventListener("dragstart", function (e) {
+    e.preventDefault();
+})
+
+//鼠标按下移动松开停止的功能
+spot.addEventListener("mousedown", function (e) {
+    function move4() {
+        move3(event, e.offsetX);
+    }
+    window.addEventListener("mousemove", move4);
+    window.addEventListener("mouseup",function () {
+        window.removeEventListener("mousemove",move4);
+    })
+});
+
 
 //"red"
 
 
-
+*/
 
