8506ec50a0c72971f557a87845d3b8f1a0b3db49
Lisa <857871573@qq.com>
2016-09-10 12:17:19 +0800

    解决了旋转问题

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,8 @@
  * Created by lisa on 16/9/4.
  */
 
-var element = document.getElementById("object");
+//移动物体的功能
+var element = document.getElementById("element");
 //var canvas = document.getElementById("canvas");
 var canvas= document.getElementById("canvas");
 //test.offsetLeft是移动的相对横坐标
@@ -11,10 +12,7 @@ var canvas= document.getElementById("canvas");
 //offsetTop是点击的位置相对被点击对象的纵坐标
 //clientX是点击的那个点的横坐标
 //clientY是点击的那个点的纵坐标
-
-
-//移动物体的功能
-function move(event, offsetLeft, offsetTop) {
+function move1(event, offsetLeft, offsetTop) {
     var x = event.clientX-offsetLeft-3-canvas.offsetLeft;
     if(x > 570) {
         x = 570;
@@ -28,7 +26,9 @@ function move(event, offsetLeft, offsetTop) {
         y = 0;
     }
     //console.log(x, y)
-    element.style.transform = "translate("+x+"px,"+y+"px)";
+    //element.style.transform = "translate("+x+"px,"+y+"px)";
+    element.style.top = y + "px";
+    element.style.left = x + "px";
 }
 //阻挡原始拖动的产生
 element.addEventListener("dragstart", function (e) {
@@ -37,8 +37,7 @@ element.addEventListener("dragstart", function (e) {
 //鼠标按下移动松开停止的功能
 element.addEventListener("mousedown", function (e) {
     function move2() {
-        move(event, e.offsetX, e.offsetY);
-
+        move1(event, e.offsetX, e.offsetY);
     }
     window.addEventListener("mousemove", move2);
     window.addEventListener("mouseup",function () {
@@ -47,6 +46,50 @@ element.addEventListener("mousedown", function (e) {
  });
 
 
+//旋转物体的功能
+var rotate = document.getElementById("rotate");
+function romate1(event, rotate) {
+    var x2 = event.clientX-canvas.offsetLeft;
+    element.style.transform = "rotate(" + 10 + "deg)";
+    console.log(element.style.transform)
+    /*var x1 = element.offsetLeft;
+    var y1 = element.offsetTop;
+    var x2 = event.clientX-offsetLeft-3-canvas.offsetLeft;
+    var y2 = event.clientY-3-offsetTop-canvas.offsetTop;
+    var x = Math.abs(x1-x2);
+    var y = Math.abs(y1-y2);
+    var z = Math.sqrt(x*x+y*y);
+    var rotat = Math.round((Math.asin(y/z)/Math.PI*180));
+// 第一象限
+    if (x2 >= x1 && y2 <= y1) {
+        rotat = rotat;
+    }
+// 第二象限
+    else if (x2 <= x1 && y2 <= y1) {
+        rotat = 180 - rotat;
+    }
+// 第三象限
+    else if (x2 <= x1 && y2 >= y1) {
+        rotat = 180 + rotat;
+    }
+// 第四象限
+    else if(x2 >= x1 && y2 >= y1){
+        rotat = 360 - rotat;
+    }
+    return rotat; //真实的角度
+    element.style.transform = "rotate(" + rotate + "deg)";*/
+}
+//鼠标按下移动松开停止的功能
+rotate.addEventListener("mousedown", function (event) {
+    function rotate2(event) {
+        romate1(event,rotate);
+    }
+    window.addEventListener("mousemove", rotate2);
+    window.addEventListener("mouseup",function () {
+        window.removeEventListener("mousemove",rotate2);
+    })
+});
+
 //每点击一下就增加一个圆圈;
 var middle = document.getElementById("middle");
 var slider = document.getElementById("slider");
@@ -54,7 +97,7 @@ var animate = document.getElementById("animate");
 var event = slider.event;
 //移动圆圈的功能
 function move3(event, offsetLeft, offsetTop, circle) {
-    console.log(event.clientX, 1)
+    //console.log(event.clientX, 1)
     var x = event.clientX-offsetLeft-animate.offsetLeft-middle.offsetLeft;
     if(x > 595) {
         x = 595;
@@ -87,7 +130,6 @@ function generate(event) {
     var x = event.offsetX - 5;
     circle.style.left = x + "px";
     middle.appendChild(circle);
-
 };
 
 slider.addEventListener("click", function (event) {
