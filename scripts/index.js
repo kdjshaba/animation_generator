25544ac29fa340040bf0f7a72f24bff5caa1fe06
Lisa <857871573@qq.com>
2016-09-18 11:12:31 +0800

    给每个circle加时间(没有加circle移动和transform)

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,19 @@
  * Created by lisa on 16/9/4.
  */
 
+var arr = new Array();
+
+
+
+
+
+
+
 //移动物体的功能
 var element = document.getElementById("element");
 //var canvas = document.getElementById("canvas");
 var canvas= document.getElementById("canvas");
+var object = document.getElementById("object");
 //test.offsetLeft是移动的相对横坐标
 //test.offsetTop是移动的相对纵坐标
 //offsetLeft是点击的位置相对被点击对象的横坐标
@@ -35,7 +44,8 @@ element.addEventListener("dragstart", function (e) {
     e.preventDefault();
 })
 //鼠标按下移动松开停止的功能
-element.addEventListener("mousedown", function (e) {
+object.addEventListener("mousedown", function (e) {
+    //alert(1)
     function move2() {
         move1(event, e.offsetX, e.offsetY);
     }
@@ -48,14 +58,16 @@ element.addEventListener("mousedown", function (e) {
 
 //旋转物体的功能
 var rotate = document.getElementById("rotate");
+var xxx = 0;
 function romate1(event, rotate) {
+xxx ++;
     var x2 = event.clientX-canvas.offsetLeft;
-    element.style.transform = "rotate(" + 10 + "deg)";
+
     console.log(element.style.transform)
-    /*var x1 = element.offsetLeft;
+    var x1 = element.offsetLeft;
     var y1 = element.offsetTop;
-    var x2 = event.clientX-offsetLeft-3-canvas.offsetLeft;
-    var y2 = event.clientY-3-offsetTop-canvas.offsetTop;
+    var x2 = event.clientX-3-canvas.offsetLeft;
+    var y2 = event.clientY-3-canvas.offsetTop;
     var x = Math.abs(x1-x2);
     var y = Math.abs(y1-y2);
     var z = Math.sqrt(x*x+y*y);
@@ -76,8 +88,9 @@ function romate1(event, rotate) {
     else if(x2 >= x1 && y2 >= y1){
         rotat = 360 - rotat;
     }
-    return rotat; //真实的角度
-    element.style.transform = "rotate(" + rotate + "deg)";*/
+    //return rotat; //真实的角度
+    element.style.transform = "rotate(" + (-rotat) + "deg)";
+    element.style.transformOrigin = "bottom bottom";
 }
 //鼠标按下移动松开停止的功能
 rotate.addEventListener("mousedown", function (event) {
@@ -108,7 +121,13 @@ function move3(event, offsetLeft, offsetTop, circle) {
     //console.log(x, y)
     circle.style.left = x+"px";
 }
-//产生圆圈的功能
+
+
+/**
+ * 产生圆圈的功能
+ * @param event
+ */
+
 function generate(event) {
     var circle = document.createElement("div");
     //阻挡原始拖动的产生
@@ -130,20 +149,27 @@ function generate(event) {
     var x = event.offsetX - 5;
     circle.style.left = x + "px";
     middle.appendChild(circle);
+
+    var time = event.offsetX/600;
+    var inserted = false;
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i].time > time) {
+            arr.splice(i, 0, {time: time,offsetX:event.offsetX });
+            inserted = true;
+            break;
+        }
+    }
+    if(inserted == false) {
+        arr.push({
+            time: time,
+            offsetX: event.offsetX
+        })
+    }
+
 };
 
 slider.addEventListener("click", function (event) {
     generate (event);
 });
 
-
-//可以移动圆圈的功能
-//var spot = document.getElementByClassName("circle")
-
-
-
-
-
-
-//"red"
 
